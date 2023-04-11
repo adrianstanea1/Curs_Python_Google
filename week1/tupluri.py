8cc9dacbdebe7492757cbfbe10d7294d3bf83b69
Bogdan Stanea <Stanea.Ni.Adrian@student.utcluj.ro>
2023-04-11 18:01:03 +0300

    Sedinta 2: code along

---
+++ week1/tupluri.py
@@ -0,0 +1,7 @@
+tuplu1 = (1,7, -3)
+tuplu2 = ('hello', None)
+
+print(type(tuplu1))
+print(tuplu1 + tuplu2)
+
+# OBS: doar o variabila intre () va converti in tipul sau; nu mai este tuplu
---
+++ week1/variabile.py
@@ -0,0 +1,16 @@
+a = '3'
+a += '0'
+print(a)
+
+nr_mere = 2
+nr_pere = 3
+# mesaj = f"Ana are {nr_mere} mere si {nr_pere} pere"
+# mesaj = "Ana are {0} mere si {1} pere".format(nr_mere, nr_pere) # pe baza index din lista de argumente se asociaza pozitiilorr puse in {}
+# mesaj = f"Ana are '{nr_mere}' mere si '{nr_pere}' pere"
+mesaj = f"Ana are \"{nr_mere}\" mere si \"{nr_pere}\" pere"
+mesaj = """Ana are mere
+si pere
+"""
+mesaj = """Ana are mere
+"""
+print(mesaj)
